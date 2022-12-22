79591a5ee900570e669686cdeebba5651fdeed49
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-12-22 11:39:04 -0300

    commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { VueMaskDirective } from "v-mask";
-
-Vue.config.productionTip = false;
-
-Vue.directive("mask", VueMaskDirective);
-
-new Vue({
-    render: h => h()
-}).$mount("#app")
+import VueMask from 'v-mask'
+Vue.use(VueMask);
 
 
