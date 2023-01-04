c10f8bfbbcd8deb6cb69c8785976d65f79cf5704
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-04 08:47:48 -0300

    Alterando formulario de Endereço

--- src/components/informacoes_de_Endereco.vue
+++ src/components/informacoes_de_Endereco.vue
@@ -4,13 +4,13 @@
 
         <div id="endereco_principal">
             <h2 class="h2_endereco">Endereço principal</h2>    
-            <label class="lb_cep">Seu CEP:  </label><input type="text" class="input_cep" >
-            <input type="text" placeholder="Endereço" class="input_endereco">
-            <input type="text" placeholder="Numero" class="input_endereco">
-            <input type="text" placeholder="Complemento(opcional)" class="input_endereco">
-            <input type="text" placeholder="Bairro" class="input_endereco">
-            <input type="text" placeholder="Cidade" class="input_endereco">
-            <input type="text" placeholder="Estado" class="input_endereco">
+            <label class="lb_cep">Seu CEP:</label> <input type="text" class="input_cep" v-model="cep" v-on:click="consulta_cep">
+            <input type="text" placeholder="Endereço" class="input_endereco" v-model="Endereco">
+            <input type="text" placeholder="Numero" class="input_endereco" v-model="numero">
+            <input type="text" placeholder="Complemento(opcional)" class="input_endereco" v-model="Complemento">
+            <input type="text" placeholder="Bairro" class="input_endereco" v-model="cep_data.bairro">
+            <input type="text" placeholder="Cidade" class="input_endereco" v-model="cep_data.localidade">
+            <input type="text" placeholder="Estado" class="input_endereco" v-model="cep_data.uf">           
         </div>
     
         <div id="endereco_secundario">
@@ -45,11 +45,38 @@
 </template>
 <script>
 import axios from 'axios';
+import { assertExpressionStatement } from '@babel/types';
+
 
  export default{
     name: 'Informacoes_Endereco',
+    data(){
+        return{
+            cep: "",
+            cep_data: "",
+            cep_keys: []
+        }
+    },
 
     methods: {
+        consulta_cep(){
+            var self = this;
+
+            axios
+            .get('https://viacep.com.br/ws/'+this.cep+'/json/')
+            .then(function (response) {
+
+                console.log(response);
+                self.cep_data = response.data;
+                self.cep_keys = Object.keys( self.cep_data );
+            })
+            .catch(function (error){
+                //console.log(error);
+            })
+            .finally(function () {
+            });
+        },
+
         Cadastro_Informacoes(){
             axios({
                 method: 'post',
