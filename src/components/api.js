5b4a6b507caf814d50033aeef3a9a683f214cb28
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-23 13:02:38 -0300

    concertando erros

--- src/components/api.js
+++ src/components/api.js
@@ -12,10 +12,8 @@ api.interceptors.request.use(
         const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJULkkuIEdlc3RvciIsInN1YiI6IjUwNWJhNDJlYTQ1NTUzNzYwNzkwMjk4NDc4ZDJmYmY0ZDA3OTFhMDIiLCJleHAiOjE2NzQ1NjAwODZ9.WoLxStS3o_QS1kfw9DA-rFOiw8JaPyrMyLXobHVNL7E"
 
         config.headers.Authorization = `Bearer ${token}`;
+
         return config;
-    },
-    (error) => {
-        return Promise.reject(error);
     }
 );
 
