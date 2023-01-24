12c8cd1c49dc6629a8fe293a2ba61f96d230fb92
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-24 08:36:15 -0300

    Consertando erros

--- src/components/Pessoa_Fisica/Informacoes_Cliente.vue
+++ src/components/Pessoa_Fisica/Informacoes_Cliente.vue
@@ -4,21 +4,23 @@
 
     <h2 id="h2-informacoes">Informações do cliente</h2>
 
-    <form method="POST" @click="Cadastrar">
+    <form method="POST" @submit.prevent="Cadastrar">
 
         <input type="text" name="nome" placeholder="Seu Nome" v-model="nome" class="input" required>
         <div id="nome_apelido" data-text="Nome Apelido">
             <input type="text" placeholder="nome-apelido" v-model="nome_fantasia">
         </div>
         <input type="text" placeholder="Seu email principal" class="input" v-model="e_mail">
+
         <input type="text"  placeholder="Seu email nfe" class="input" v-model="e_mail_nfe">
 
-        <input type="text" placeholder="Data do cadastro" class="input" v-model="data_cadastro">
+        <input type="text" placeholder="Codigo do cliente" class="input" v-model="cod_cliente">
 
-        <!--<button type="button" @click="input++" class="btn_adicionar_email" id="send">
-            Adicionar outro email
-            <img src="src/assets/images/icon_add.png" />
-        </button>-->
+        <input type="text"  placeholder="Codigo do vendedor" class="input" v-model="cod_vendedor">
+
+        <input type="text" placeholder="Classificação de entrega" v-model="classificacao_entrega">
+    
+        <input type="text" placeholder="Data do cadastro" class="input" v-model="data_cadastro">
 
         <input type="text" placeholder="Seu telefone" class="input" v-model="telefone_2" />
         <input type="text" placeholder="Celular" class="input" v-model="celular" />
@@ -26,26 +28,23 @@
             <input type="text" placeholder="Seu CPF" class="input" v-model="cnpj_cpf" />
         </div>
 
-        <br><input type="text"  placeholder="CPF de entrega" style="display: inline" class="input_separado_rg" v-model="cnpj_cpf_entrega"><br><br>
+        <br><input type="text" placeholder="CPF de entrega" style="display: inline" class="input_separado_rg" v-model="cnpj_cpf_entrega"><br><br>
 
         <br><label class="lb_rg">Seu RG:</label><input type="text" style="display: inline" class="input_separado_rg" v-model="inscricao_rg"><br><br>
 
-        <br><label class="lb_dt">Data de Nascimento:</label><input type="date" style="display: inline"
-            class="input_secundario_data" v-model="nascimento">
+        <br><label class="lb_dt">Data de Nascimento:</label><input type="date" style="display: inline" class="input_secundario_data" v-model="nascimento">
 
         <!--Tipo Suframa-->
 
         <h2 class="h2_suframa">Tipo <strong>SUFRAMA</strong>(Superintendência da<br>Zona Franca de Manaus)</h2>
         <br>
         <select class="select" v-model="suframa_tipo">
-            <option>ZFM(Zona Franca de Manaus)</option>
-            <option>ALC(Área de Livre Comercio)</option>
-            <option>Am. Ocid(Amazonia Ocidental)</option>
+            <option value="0">ZFM(Zona Franca de Manaus)</option>
+            <option value="1">ALC(Área de Livre Comercio)</option>
+            <option value="2">Am. Ocid(Amazonia Ocidental)</option>
         </select>
 
         <br><label class="label_codigo_suframa">Codigo SUFRAMA:</label><input type="text" style="display: inline">
-
-            <br><input type="text" style="display: inline" class="suframa_tipo"  placeholder="Tipo Suframa" v-model="suframa_tipo"><br>
 
         <br><label class="label_num_inscricao">Numero de inscrição municipal: </label><input type="text" style="display: inline" class="input_inscricao" v-model="inscricao_municipal">
 
@@ -58,7 +57,9 @@
         </textarea>
 
         <div id="endereco_principal">
+
             <h2 class="h2_endereco">Endereço principal</h2>
+
             <label class="lb_cep">Seu CEP:</label> <input type="text" class="input_cep" v-model="cep">
             <input type="text" placeholder="Endereço" class="input_endereco" v-model="endereco">
             <input type="text" placeholder="Numero" class="input_endereco" v-model="end_numero">
@@ -89,6 +90,8 @@ export default {
             nome_fantasia: "",
             classificacao: "1",
             classificacao_entrega: "",
+            cod_cliente: "", 
+            cod_vendedor: "",
             e_mail: "",
             observacao: "",
             e_mail_nfe: "",
@@ -128,6 +131,8 @@ export default {
                     nome_fantasia: this.nome_fantasia,
                     classificacao: this.classificacao,
                     classificacao_entrega: this.classificacao_entrega,
+                    cod_cliente: this.cod_cliente, 
+                    cod_vendedor: this.cod_vendedor,
                     e_mail: this.e_mail,
                     observacao: this.observacao,
                     e_mail_nfe: this.e_mail_nfe,
