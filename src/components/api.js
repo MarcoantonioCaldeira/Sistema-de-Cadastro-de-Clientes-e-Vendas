831921539925169e19d0e2fedadebf91f22e4f36
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-30 11:43:14 -0300

    Adicionando novas funcionalidades

--- src/components/api.js
+++ src/components/api.js
@@ -5,17 +5,25 @@ const api = axios.create({
 });
 
 
-api.interceptors.request.use((request) => {
-
-    const token = localStorage.getItem("token");
+options.interceptors.request.use((request) => {
 
     if (token) {
 
         request.headers.Authorization = `Bearer ${token}`;
-
     }
     return request;
-
 });
+
+const options = {
+    method: 'GET',
+    url: 'http://localhost:9000/auth',
+    params: { '': ['', ''] },
+    headers: {
+        'Content-Type': 'application/json',
+        key_auth: '3G5T8W7Y1K',
+        SYSDBA: 'masterkey'
+    }
+};
+
 
 export default api;
