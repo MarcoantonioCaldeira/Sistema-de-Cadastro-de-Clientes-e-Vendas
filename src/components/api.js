b824635c623f91f3bb8f3700c1176d7e7884d429
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-01-31 12:49:30 -0300

    Adicionando Requisicao Token

--- src/components/api.js
+++ src/components/api.js
@@ -1,23 +1,8 @@
 import axios from "axios";
-import common from "./http-common";
+
 
 const api = axios.create({
     baseURL: "http://localhost:9000",
 });
 
 
-
-common.interceptors.request.use((request) => {
-
-    const token = localStorage.getItem("token");
-
-    if (token) {
-
-        request.headers.Authorization = `Bearer ${token}`;
-
-    }
-    return request;
-
-});
-
-export default api;
