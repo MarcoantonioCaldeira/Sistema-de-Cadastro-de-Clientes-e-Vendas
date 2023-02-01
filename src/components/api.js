9014a3221c255aeb439352dcbc38ff75965b904e
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-02-01 08:09:12 -0300

    commit

--- src/components/api.js
+++ src/components/api.js
@@ -5,25 +5,24 @@ const api = axios.create({
 });
 
 
-options.interceptors.request.use((request) => {
+// options.interceptors.request.use((request) => {
 
-    if (token) {
+//     if (token) {
 
-        request.headers.Authorization = `Bearer ${token}`;
-    }
-    return request;
-});
+//         request.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return request;
+// });
 
-const options = {
-    method: 'GET',
-    url: 'http://localhost:9000/auth',
-    params: { '': ['', ''] },
-    headers: {
-        'Content-Type': 'application/json',
-        key_auth: '3G5T8W7Y1K',
-        SYSDBA: 'masterkey'
-    }
-};
-
+// const options = {
+//     method: 'GET',
+//     url: 'http://localhost:9000/auth',
+//     params: { '': ['', ''] },
+//     headers: {
+//         'Content-Type': 'application/json',
+//         key_auth: '3G5T8W7Y1K',
+//         SYSDBA: 'masterkey'
+//     }
+// };
 
 export default api;
