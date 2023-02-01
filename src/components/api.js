523a9cef39de9efc1e71102bba7024f054b49bdf
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-02-01 11:53:34 -0300

    Adicionando Refresh Token

--- src/components/api.js
+++ src/components/api.js
@@ -7,27 +7,25 @@ const api = axios.create({
 
 
 
-const instance = axios.create()
 
+// instance = axios.create({
+//     baseURL: "http://localhost:9000/auth",
+//     params: { '': ['', ''] },
+//     headers: {
+//         'Content-Type': 'application/json',
+//         key_auth: '3G5T8W7Y1K',
+//         SYSDBA: 'masterkey'
+//     },
 
-instance = axios.create({
-    baseURL: "http://localhost:9000/auth",
-    params: { '': ['', ''] },
-    headers: {
-        'Content-Type': 'application/json',
-        key_auth: '3G5T8W7Y1K',
-        SYSDBA: 'masterkey'
-    },
+// })
 
-})
+// instance.interceptors.request.use((request) => {
 
-instance.interceptors.request.use((request) => {
+//     const token = localStorage.getItem("Authorization");
 
-    const token = localStorage.getItem("Authorization");
+//     if (token) {
+//         request.headers.Authorization = `Bearer ${token}`;
+//     }
 
-    if (token) {
-        request.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return request;
-})
+//     return request;
+// })
