e3f16b6292397dc9b28e45681a77b903c9a42b83
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-02-14 08:51:40 -0300

    Adicionando Bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 createApp(App).use(router).mount('#app')
 
-import { VueMaskDirective } from "v-mask";
-Vue.directive("mask", VueMaskDirective);
+import { VueMaskDirective } from "v-mask"
+Vue.directive("mask", VueMaskDirective)
