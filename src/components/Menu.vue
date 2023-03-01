ae2105a2a075e9ee999dfd7855543d9ec6604742
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-01 10:34:31 -0300

    Deixando a navbar responsiva

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,32 +1,37 @@
 <template> 
-   
-    <div id="Menu_Sistema" class="meu_menu">
-        <i class="fas fa-bars"   @click="Mostrar_Menu"></i> 
+    <div>
+        <div class="nav-menu">
+            <i class="fas fa-bars" @click="Mostrar_Menu"> 
+            </i> 
+            <div class="nav-content" :class="this.MobileMenu ? 'abrir-menu' : 'fechar-menu'"> 
 
-        <div class="nav-content" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'"> 
-            <router-link to="/"><li>Inicio</li></router-link>
-            <router-link to="./Selecao_Tipo_Pessoa"><li>Cadastro de Clientes</li></router-link>
-            <router-link to="./Cadastro_De_Vendas"><li>Cadastro de Vendas</li></router-link>
+                <ul class="nav-items">
+                    <router-link to="/"><li>Inicio</li></router-link>
+                    <router-link to="./Selecao_Tipo_Pessoa"><li>Cadastro de Clientes</li></router-link>
+                    <router-link to="./Cadastro_De_Vendas"><li>Cadastro de Vendas</li></router-link>
+                    
+                </ul>
+                <router-view/>
+            </div>
         </div>
     </div>
-   
-    <router-view/>
 </template>
 
 <script> 
 export default{
-    name: 'Menu_Sistema',
 
-    data(){
-        return{
-            showMobileMenu: false
-        }
-    },
-    methods: {
-        Mostrar_Menu(){
-            this.showMobileMenu = !this.showMobileMenu;
-        }
-    }
+data(){
+  return{
+    MobileMenu: false,
+  }
+},
+
+methods: {
+  Mostrar_Menu() {
+    this.MobileMenu = !this.MobileMenu;
+  }
+}
+
 }
 
 
