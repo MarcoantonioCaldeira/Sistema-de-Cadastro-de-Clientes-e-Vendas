59b56b1b35b7823c9b3f9e85118c5bf3543ee791
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-03 09:13:37 -0300

    Criando Menu responsivo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,22 @@
 <template> 
-    <div>
-        <div class="nav-menu">
-            <i class="fas fa-bars" @click="Mostrar_Menu"> 
-            </i> 
-            <div class="nav-content" :class="this.MobileMenu ? 'abrir-menu' : 'fechar-menu'"> 
-
-                <ul class="nav-items">
-                    <router-link to="/"><li>Inicio</li></router-link>
-                    <router-link to="./Selecao_Tipo_Pessoa"><li>Cadastro de Clientes</li></router-link>
-                    <router-link to="./Cadastro_De_Vendas"><li>Cadastro de Vendas</li></router-link>       
-                </ul>
-                <router-view/>
-            </div>
-        </div>
+    <div class="App">
+       <Menu />
     </div>
+    <router-view />
 </template>
 
 <script> 
+
+import Menu from './components/Menu.vue'
+
 export default{
-
-  data(){
-    return{
-      MobileMenu: false,
-    }
-  },
-
-  methods: {
-    Mostrar_Menu() {
-      this.MobileMenu = !this.MobileMenu;
-    }
+  name: 'App',
+  components:{
+    Menu
   }
-}
+};
 </script>
 
-<style src="./estilo_app.css"> 
+<style> 
 
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,40 +1,91 @@
 <template> 
+<header :class="{ 'scrolled-nav': scrollPosition }">
+  <nav>
     <div>
-        <div class="nav-menu">
-            <i class="fas fa-bars" @click="Mostrar_Menu"> 
-            </i> 
-            <div class="nav-content" :class="this.MobileMenu ? 'abrir-menu' : 'fechar-menu'"> 
-
-                <ul class="nav-items">
-                    <router-link to="/"><li>Inicio</li></router-link>
-                    <router-link to="./Selecao_Tipo_Pessoa"><li>Cadastro de Clientes</li></router-link>
-                    <router-link to="./Cadastro_De_Vendas"><li>Cadastro de Vendas</li></router-link>
-                    
-                </ul>
-                <router-view/>
-            </div>
-        </div>
+      
     </div>
+    <ul>
+      <li><router-link to="">nicio</router-link></li>
+      <li><router-link to="">Cadastro de Clientes</router-link></li>
+      <li><router-link to="">Cadastro de Vendas</router-link></li>
+    </ul>
+    <div class="icon">
+      <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{ 'icon-active': mobileNav}"></i>
+    </div>
+    <transition name="mobile-nav">
+      <ul v-show="mobileNav" class="dropdown-nav">
+        <li><router-link to="">Inicio</router-link></li>
+        <li><router-link to="">Cadastro de Clientes</router-link></li>
+        <li><router-link to="">Cadastro de Vendas</router-link></li>
+      </ul>
+    </transition>
+  </nav>
+</header>
 </template>
 
 <script> 
 export default{
-
-  data(){
-    return{
-      MobileMenu: false,
-    }
-  },
-
-  methods: {
-    Mostrar_Menu() {
-      this.MobileMenu = !this.MobileMenu;
-    }
-  }
+  name: 'Menu'
 
 }
 </script>
 
-<style src="../estilo_app.css"> 
+<style lang="scss" scoped> 
 
+header {
+  background-color: red;
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: a.5s ease all;
+  color: #fff;
+
+nav {
+  display: flex;
+  flex-direction: row;
+  padding: 12px 0;
+  transition: 0.5s ease all;
+  width: 90%;
+  margin: 0 auto;
+  @media (min-width: 1140px) {
+      max-width: 1140px;
+  }
+
+      ul
+      .link {
+          font-weight: 500;
+          color: #fff;
+          list-style: none;
+          text-decoration: none;
+      }
+
+      li {
+          text-transform: uppercase;
+          padding: 16px;
+          margin-left: 16px;
+      }
+
+      .link {
+          font-size: 14px;
+          transform: .5s ease all;
+          padding-bottom: 4px;
+          border-bottom: 1px solid transparent;
+
+          &:hover {
+              color: blue;
+              border-color: blue;
+          }
+      }
+
+    .branding {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 50px;
+        transition: 0.5s ease all;
+      }
+    } 
+  }
+}
 </style>
