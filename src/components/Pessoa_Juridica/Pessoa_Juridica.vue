a5c1d5aafc54b81ef927581ba0940f9d7fe9fe12
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-13 13:08:26 -0300

    Resolvendo conflitos de SCSS

--- src/components/Pessoa_Juridica/Pessoa_Juridica.vue
+++ src/components/Pessoa_Juridica/Pessoa_Juridica.vue
@@ -7,6 +7,26 @@
 <script>
 
 </script>
-<style src="./estilo_p_juridica/estilo_inicio_p_juridica.scss"  lang="scss">
+<style  lang="scss">
+@import "@/assets/scss/index.scss";
+
+#Area_Pessoa_Juridica {
+    width: 20%;
+    margin-left: 40%;
+    margin-top: 3%;
+    height: 90px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    font-family: $font_2;
+    background-color: $cor_4;
+    border-radius: 5px;
+  }
+
+  .h2_p_juridica {
+    color: $cor_3;
+    position: relative;
+    margin-left: 28%;
+    top: 28%;
+    text-decoration: none;
+  }
 
 </style>
