c9499f6fab676bef21eab3901f12bb76e981482c
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-03-15 08:58:04 -0300

    Resolvendo problemas da api

--- src/components/api.js
+++ src/components/api.js
@@ -1,24 +1,57 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-    headers: {
-        'Content-Type': 'application/json',
-        key_auth: '3G5T8W7Y1K',
-        SYSDBA: 'masterkey'
-    },
-    baseURL: process.env.API_URL,
+    baseURL: 'https://localhost:9000/auth',
 });
 
-api.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async function (error) {
-        const access_token = localStorage.getItem("access_token");
-        if (error.response.status === 401 && access_token) {
-            const response = await refreshToken(error);
-            return response;
+api.interceptors.request.use(
+
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
+        return config;
+    },
+
+    (error) => {
         return Promise.reject(error);
     }
 );
+
+api.interceptors.response.use(
+
+    (response) => {
+        return response;
+    },
+
+    (error) => {
+
+        const originalRequest = error.config;
+        if (
+            error.response.status === 401 &&
+            !originalRequest._retry &&
+            localStorage.getItem('refreshToken')
+        ) {
+            originalRequest._retry = true;
+            return api
+                .post('/refresh-token', {
+                    refreshToken: localStorage.getItem('refreshToken'),
+                })
+                .then((response) => {
+                    localStorage.setItem('token', response.data.token);
+                    localStorage.setItem('refreshToken', response.data.refreshToken);
+                    api.defaults.headers.common[
+                        'Authorization'
+                    ] = `Bearer ${response.data.token}`;
+                    return api(originalRequest);
+                });
+        }
+        return Promise.reject(error);
+    }
+
+);
+
+export default api;
+
+
