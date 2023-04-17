8983a0ad002227c9ebf66df78a628469a61d5b16
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-04-17 10:35:57 -0300

    fezendo testes com API

---
+++ src/mixins/session.js
@@ -0,0 +1,11 @@
+export const mxSession =  {
+    data(){
+        return{
+          user:{      
+            token : '',  
+          },
+         }
+        }
+      }  
+  export default mxSession;    
+      
