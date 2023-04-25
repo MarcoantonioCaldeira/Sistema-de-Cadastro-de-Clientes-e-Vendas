2e896069f872fe9e81c950a627eae35a5a3ad207
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-04-25 13:32:47 -0300

    fazendo testes na api

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,61 +1,27 @@
 import axios from 'axios';
 
 
+
 const api = axios.create({ 
     baseURL: 'http://localhost:9000'
 });
 
+axios.interceptors.request.use(function(config){
 
-api.interceptors.request.use((request) => {
-        
-    const token =  localStorage.getItem('token');
-           
-    if (token) {
-        request.headers.Authorization = `Bearer ${token}`;
-    }
-    
-    return request;
-});
+  config.headers.common.Authorization = 'Token';
 
-api.interceptors.response.use((response) => {
-  
-    return response;
+  return config;
 
-}, async (error) => {
-    
-    const originalRequest = error.config;
-    
-    if (error?.response?.status === 401 && 
-        !originalRequest?.__isRetryRequest
-    ){
+}, function (error){
 
-        originalRequest.retry = true;
-        
-        const refreshToken = localStorage.getItem("refreshToken");
+    console.log(error);
 
-        
-        if(!refreshToken) {
-
-            localStorage.clear();
-
-            return (window.location.href = "/");
-        }
-        
-        const response = await refresh(refreshToken);
-
-        const data = {
-            // accessToken = response.token;
-            // refreshToken = response.refreshToken;
-        };
-       
-        localStorage.setItem(JSON.stringify(data), "refreshToken");
-   
-        return api(originalRequest);
-    }
-   
     return Promise.reject(error);
 });
 
+
+
+   
 
 
 export default api;
